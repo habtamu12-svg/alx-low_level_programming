b0c7b47725abb3a5b6aecb1aeafa2b0c21597cdc
habtamu12-svg <agsehagis@gmail.com>
2022-09-08 22:14:31 -0700

    ”programin_puzzel”

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>                                                                                                                                                                     
-/**                                                                                                                                                                                    
+/**                                                                          
  * main - Prints onto screen
  *
  * Return: zero
