502ff69585db453f725974e14d1a43c8dd3fbf35
habtamu12-svg <agsehagis@gmail.com>
2022-09-08 23:11:09 -0700

    Size_vtype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * main - computes sizes of various data types
+ * main - Prints the size of many var types
  *
- * Return: zero
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
 
-	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
-	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
-	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
-	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
-	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }	
