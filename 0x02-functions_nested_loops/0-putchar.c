304bc971b7d9c4946038776dc690a18a77e60755
habtamu12-svg <agsehagis@gmail.com>
2022-09-13 07:35:10 -0700

    ”pp”

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,41 @@
-#include "holberton.h"
+nclude "_putchar.h"
+
+
 /**
- * main - Entry point
- * Description: prints 'Holberton \n'
- * Return: Always 0 (success)
- */
+ *
+ * * main - Entry point
+ *
+ * * Description: prints '_putchar \n'
+ *
+ * * Return: Always 0 (success)
+ *
+ * */
+
 int main(void)
+
 {
-char str[] = "Holberton\n";
-int i = 0;
 
-while (str[i] != '\0')
+	char str[] = "_putchar\n";
+
+	int i = 0;
+
+
+
+	while (str[i] != '\0')
+
 	{
-	char c = str[i];
 
-	_putchar(c);
-	i++;
+		char c = str[i];
+
+
+
+		_putchar(c);
+
+		i++;
+
 	}
-return (0);
+
+	return (0);
+
 }
+
