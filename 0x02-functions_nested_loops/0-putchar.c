7635c8d6e8a508d2cccebd54058a2e7b13254a2c
habtamu12-svg <agsehagis@gmail.com>
2022-09-13 09:53:16 -0700

    ”pp”

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
  * main - print putchar
- *
- * Return: Always 0 (success)
+ * Return: always 0
  */
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+
+ int main(void)
+ {
+     _putchar('_');
+     _putchar('p');
+     _putchar('u');
+     _putchar('t');
+     _putchar('c');
+     _putchar('h');
+     _putchar('a');
+     _putchar('r');
+     _putchar('\n');
 
      return (0);
-}
+
+ }
