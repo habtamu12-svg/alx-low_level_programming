6ef84e407080a471b29f0f2e399d53cd0f7e52c2
habtamu12-svg <agsehagis@gmail.com>
2022-09-15 23:43:28 +0300

    print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
+ * print_number - prints an interger.
+ *
+ * @n: number.
  */
-
 void print_number(int n)
 {
-	unsigned int x;
-
-	x = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		_putchar('-');
+		n = -n;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
