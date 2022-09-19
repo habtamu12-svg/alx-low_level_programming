db459b675c4756c7d5f0e322f294e17d10fc18cc
habtamu12-svg <agsehagis@gmail.com>
2022-09-19 19:51:38 +0300

    printOtherchar

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,32 @@
-#incldue "main.h"
+#include "main.h"
 
 /**
- * puts2 - prints xters of a string
- * @str: the used string reference
- * Return: 0
+ * puts2 - print every other character of a string.
+ * @str: character
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; i < string_length(str); i += 2)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
