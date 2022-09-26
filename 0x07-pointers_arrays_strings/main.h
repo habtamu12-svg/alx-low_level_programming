fac68fbdc57742e98128e258ff1a5d71b006344e
habtamu12-svg <agsehagis@gmail.com>
2022-09-26 13:32:52 +0300

    nn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,17 +2,16 @@
 #define MAIN_H
 
 /**
-* File: main.h
-* Auth: Habtamu
-* Desc: Header file containing prototypes for all functions
-* used in the 0x07-pointers_arrays_strings directory.
-*/
+ * File :main
+ * Auth : Habtamu
+ * description: Header file containing prototype for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
 
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -22,4 +21,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
-
